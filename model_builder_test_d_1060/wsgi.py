fc45696d83baaa22081ee767df66c6052fce40cc
Crowdbotics <team@crowdbotics.com>
2019-11-04 19:49:33 +0000

    Create models Profile

--- model_builder_test_d_1060/wsgi.py
+++ model_builder_test_d_1060/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'model_builder_test_d_1060.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "model_builder_test_d_1060.settings")
 
 application = get_wsgi_application()
